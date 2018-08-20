ddaa69e6c5675c3d99dbe316f519884395651864
cheshireCode <dac4158@gmail.com>
2018-08-20 14:09:31 +0800

    rewrite

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 
 ### Check Avro messages of a topic
 ```
-docker exec connect kafka-avro-console-consumer --bootstrap-server broker:9092 \
+docker exec -t kx-connect kafka-avro-console-consumer --bootstrap-server broker:9092 \
     --property schema.registry.url=http://schema-registry:8081 \
     --property print.key=true   --from-beginning \
     --topic users
+```
+
+### Check messages of a topic
+```
+docker exec -t kx-connect kafka-console-consumer --bootstrap-server broker:9092 \
+    --property print.key=true   --from-beginning \
+    --topic generated.events
+```
+
+
+### List topics
+```
+docker exec -t kx-connect kafka-topics --zookeeper zookeeper:2181 --list
 ```
 
 ### Get Docker host IP
---
+++ cli/add-connectors.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+echo "Waiting for Kafka Connect to be up..."
+attempt_counter=0
+max_attempts=5
+sleep_duration=5
+until $(curl -s --output /dev/null --head --fail http://connect:8083); do
+  if [ ${attempt_counter} -eq ${max_attempts} ];
+  then
+    echo "Max attempts reached! http://connect:8083 isn't up or reachable."
+    is_service_up=1
+    break
+  else
+    printf '.'
+    attempt_counter=$((attempt_counter+1))
+    sleep $sleep_duration
+  fi
+done
+
+if [ -z ${is_service_up+x} ];
+then
+  echo "Adding connectors..."
+
+  curl -X POST -H 'Content-Type:application/json' \
+      -H 'Accept:application/json' \
+      -d @connect.events.source.elasticsearch.json \
+      http://connect:8083/connectors
+
+  curl -X POST -H "Content-Type: application/json" \
+      --data @connect.events.sink.elasticsearch.json \
+      http://connect:8083/connectors
+
+  curl -X POST -H "Content-Type: application/json" \
+      --data @connect.users.sink.elasticsearch.json \
+      http://connect:8083/connectors
+
+  curl -X POST -H "Content-Type: application/json" \
+      --data @connect.users.sink.file.json \
+      http://connect:8083/connectors
+fi
